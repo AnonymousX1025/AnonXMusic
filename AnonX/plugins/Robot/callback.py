a33223ea2161516ce6324ece31a06e61c9e6e362
ᴛʜᴇ ᴀɴᴏɴʏᴍᴏᴜs <TheAnonymous2510@gmail.com>
2022-11-03 16:52:22 +0530

    Update callback.py

--- AnonX/plugins/Robot/callback.py
+++ AnonX/plugins/Robot/callback.py
@@ -15,33 +15,12 @@ from AnonX.utils.database import (is_active_chat,
                                        music_on)
 from AnonX.utils.decorators.language import languageCB
 from AnonX.utils.formatters import seconds_to_min
-from AnonX.utils.inline.play import (panel_markup_1,
-                                          stream_markup,
+from AnonX.utils.inline.play import (stream_markup,
                                           telegram_markup)
 from AnonX.utils.stream.autoclear import auto_clean
 from AnonX.utils.thumbnails import gen_thumb
 
 wrong = {}
-
-
-@app.on_callback_query(filters.regex("PanelMarkup") & ~BANNED_USERS)
-@languageCB
-async def markup_panel(client, CallbackQuery: CallbackQuery, _):
-    await CallbackQuery.answer()
-    callback_data = CallbackQuery.data.strip()
-    callback_request = callback_data.split(None, 1)[1]
-    videoid, chat_id = callback_request.split("|")
-    chat_id = CallbackQuery.message.chat.id
-    buttons = panel_markup_1(_, videoid, chat_id)
-    try:
-        await CallbackQuery.edit_message_reply_markup(
-            reply_markup=InlineKeyboardMarkup(buttons)
-        )
-    except:
-        return
-    if chat_id not in wrong:
-        wrong[chat_id] = {}
-    wrong[chat_id][CallbackQuery.message.message_id] = False
 
 
 @app.on_callback_query(filters.regex("MainMarkup") & ~BANNED_USERS)
