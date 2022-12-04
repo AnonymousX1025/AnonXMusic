d2416a5c4bce344badb5dd624abba5de8791264f
ᴛʜᴇ ᴀɴᴏɴʏᴍᴏᴜs <TheAnonymous2510@gmail.com>
2022-12-04 11:30:56 +0530

    Update userbot.py

--- AnonX/core/userbot.py
+++ AnonX/core/userbot.py
@@ -48,7 +48,6 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("IgCutiesClub")
                 await self.one.join_chat("FallenAssociation")
                 await self.one.join_chat("DevilsHeavenMF")
             except:
@@ -79,7 +78,6 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.one.join_chat("IgCutiesClub")
                 await self.one.join_chat("FallenAssociation")
                 await self.one.join_chat("DevilsHeavenMF")
             except:
@@ -110,7 +108,6 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.one.join_chat("IgCutiesClub")
                 await self.one.join_chat("FallenAssociation")
                 await self.one.join_chat("DevilsHeavenMF")
             except:
@@ -141,7 +138,6 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.one.join_chat("IgCutiesClub")
                 await self.one.join_chat("FallenAssociation")
                 await self.one.join_chat("DevilsHeavenMF")
             except:
@@ -172,7 +168,6 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.one.join_chat("IgCutiesClub")
                 await self.one.join_chat("FallenAssociation")
                 await self.one.join_chat("DevilsHeavenMF")
             except:
