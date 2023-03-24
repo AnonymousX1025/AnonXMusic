21c61ee57b22f29ad464b04b4064d6fec359de99
ᴠɪᴋᴀꜱʜ ɢᴜᴘᴛᴛᴀ <102718297+Vikku1343@users.noreply.github.com>
2023-03-24 16:24:06 +0530

    Update start.py

--- AnonX/plugins/start.py
+++ AnonX/plugins/start.py
@@ -43,7 +43,7 @@ async def start_comm(client, message: Message, _):
         name = message.text.split(None, 1)[1]
         if name[0:4] == "help":
             keyboard = help_pannel(_)
-            await message.reply_sticker("CAACAgUAAxkBAAJE8GK4EsoLVZC2SW5W5Q-QAkaoN8f_AAL9BQACiy14VGoQxOCDfE1KKQQ")
+            await message.reply_sticker("CAACAgUAAxkBAAP1ZA7l2DspTUXgFsQwk0IcgtZ_ExEAAicEAAIaHWFXUGLw2Z6CrmgvBA")
             return await message.reply_photo(
                        photo=config.START_IMG_URL,
                        caption=_["help_1"].format(config.SUPPORT_HEHE), reply_markup=keyboard
