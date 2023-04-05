e8de8003e7f2ced90fc0544a93a19a050a95f6ad
˹ᴧɴσɴʏᴍσᴜs ꭙ˼ <TheAnonymousX1025@gmail.com>
2023-04-05 15:07:55 +0530

    Umm
    Signed-off-by: ˹ᴧɴσɴʏᴍσᴜs ꭙ˼ <TheAnonymousX1025@gmail.com>

--- AnonX/plugins/stats.py
+++ AnonX/plugins/stats.py
@@ -348,10 +348,6 @@ async def overall_stats(client, CallbackQuery, _):
     storage = call["storageSize"] / 1024
     objects = call["objects"]
     collections = call["collections"]
-    status = db.command("serverStatus")
-    query = status["opcounters"]["query"]
-    mongouptime = status["uptime"] / 86400
-    mongouptime = str(mongouptime)
     served_chats = len(await get_served_chats())
     served_users = len(await get_served_users())
     total_queries = await get_queries()
@@ -384,12 +380,10 @@ async def overall_stats(client, CallbackQuery, _):
 **sᴜᴅᴏᴇʀs:** {sudoers} 
 
       <b><u>ᴍᴏɴɢᴏ ᴅᴀᴛᴀʙᴀsᴇ</b><u/>
-**ᴜᴩᴛɪᴍᴇ:** {mongouptime[:4]} Days
 **sɪᴢᴇ:** {datasize[:6]} Mb
 **sᴛᴏʀᴀɢᴇ:** {storage} Mb
 **ᴄᴏʟʟᴇᴄᴛɪᴏɴs:** {collections}
 **ᴋᴇʏs:** {objects}
-**ǫᴜᴇʀɪᴇs:** `{query}`
 **ʙᴏᴛ ǫᴜᴇʀɪᴇs:** `{total_queries} `
     """
     med = InputMediaPhoto(media=config.STATS_IMG_URL, caption=text)
