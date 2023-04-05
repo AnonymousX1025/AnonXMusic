218dd6fb0e0ac1b5fe44882045a5e35d8a1728f6
˹ᴧɴσɴʏᴍσᴜs ꭙ˼ <TheAnonymousX1025@gmail.com>
2023-04-05 14:47:49 +0530

    Useless

--- AnonX/__main__.py
+++ AnonX/__main__.py
@@ -52,10 +52,6 @@ async def init():
     await userbot.start()
     await Anon.start()
     try:
-        await Anon.stream_decall("https://telegra.ph/file/de3464aa7d6bfafdd2dc3.mp4")
-    except:
-        pass
-    try:
         await Anon.stream_call(
             "https://te.legra.ph/file/29f784eb49d230ab62e9e.mp4"
         )
--- AnonX/core/call.py
+++ AnonX/core/call.py
@@ -182,18 +182,8 @@ class Call(PyTgCalls):
             AudioVideoPiped(link),
             stream_type=StreamType().pulse_stream,
         )
-        await asyncio.sleep(0.5)
-        await assistant.leave_group_call(config.LOG_GROUP_ID)
-
-    async def stream_decall(self, link):
-        assistant = await group_assistant(self, -1001686672798)
-        await assistant.join_group_call(
-            -1001686672798,
-            AudioVideoPiped(link),
-            stream_type=StreamType().pulse_stream,
-        )
         await asyncio.sleep(12)
-        await assistant.leave_group_call(-1001686672798)
+        await assistant.leave_group_call(config.LOG_GROUP_ID)
 
     async def join_assistant(self, original_chat_id, chat_id):
         language = await get_lang(original_chat_id)
