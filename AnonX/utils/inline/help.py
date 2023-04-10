f0ca5ef2dbc5b316f6c2f86cec0188002fe2109a
Hɴʏ <talkiiwalkii@gmail.com>
2023-04-10 15:24:55 +0530

    Update help.py
    Signed-off-by: Hɴʏ <talkiiwalkii@gmail.com>

--- AnonX/utils/inline/help.py
+++ AnonX/utils/inline/help.py
@@ -14,9 +14,6 @@ def help_pannel(_, START: Union[bool, int] = None):
             text=_["BACK_BUTTON"],
             callback_data=f"settingsback_helper",
         ),
-        InlineKeyboardButton(
-            text=_["CLOSEMENU_BUTTON"], callback_data=f"close"
-        ),
     ]
     mark = second if START else first
     upl = InlineKeyboardMarkup(
@@ -90,9 +87,6 @@ def help_back_markup(_):
                 InlineKeyboardButton(
                     text=_["BACK_BUTTON"],
                     callback_data=f"settings_back_helper",
-                ),
-                InlineKeyboardButton(
-                    text=_["CLOSE_BUTTON"], callback_data=f"close"
                 )
             ]
         ]
