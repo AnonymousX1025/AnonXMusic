45cceaf6b4612120d93896845e4005d7eb8395b7
Hɴʏ <talkiiwalkii@gmail.com>
2023-04-10 12:09:14 +0530

    Update config.py
    Signed-off-by: Hɴʏ <talkiiwalkii@gmail.com>

--- config.py
+++ config.py
@@ -76,7 +76,7 @@ clean = {}
 autoclean = []
 
 
-START_IMG_URL = getenv("START_IMG_URL", "https://te.legra.ph/file/56d1760224589ee370186.jpg")
+START_IMG_URL = getenv("START_IMG_URL", "https://te.legra.ph/file/b0be7238fc11297dbe58f.jpg")
 
 PING_IMG_URL = getenv(
     "PING_IMG_URL",
