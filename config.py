cfde712eb634940f225bb485a2012509c97d1565
aayushahu <124359102+aayushahu@users.noreply.github.com>
2023-04-16 11:07:19 +0530

    Update config.py
    Signed-off-by: aayushahu <124359102+aayushahu@users.noreply.github.com>

--- config.py
+++ config.py
@@ -25,8 +25,8 @@ UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/AnonymousX1025/AnonX
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/FallenAssociation")
-SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/DevilsHeavenMF")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/THE_MOH_MAYA")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/love_ki_duniya")
 
 SUPPORT_HEHE = SUPPORT_GROUP.split("me/")[1]
 
