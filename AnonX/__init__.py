43905f057df8b83d2170c3faa27459d9e7d7947b
UNKNOWN-MUSIC <130531308+KwsMusic@users.noreply.github.com>
2023-04-21 23:47:37 +0530

    Update __init__.py
    Signed-off-by: UNKNOWN-MUSIC <130531308+KwsMusic@users.noreply.github.com>

--- AnonX/__init__.py
+++ AnonX/__init__.py
@@ -1,8 +1,8 @@
-from AnonX.core.bot import AnonXBot
-from AnonX.core.dir import dirr
-from AnonX.core.git import git
-from AnonX.core.userbot import Userbot
-from AnonX.misc import dbb, heroku, sudo
+from AshishX.core.bot import AnonXBot
+from AshishX.core.dir import dirr
+from AshishX.core.git import git
+from AshishX.core.userbot import Userbot
+from AshishX.misc import dbb, heroku, sudo
 from aiohttp import ClientSession
 
 from .logging import LOGGER
@@ -19,7 +19,7 @@ heroku()
 sudo()
 
 # Clients
-app = AnonXBot()
+app = AshishXBot()
 
 userbot = Userbot()
 
