cf48a69fcc5d0a5408e4f4583502bb885ea0f9fe
UNKNOWN-MUSIC <130531308+KwsMusic@users.noreply.github.com>
2023-04-21 23:49:26 +0530

    Update channelplay.py
    Signed-off-by: UNKNOWN-MUSIC <130531308+KwsMusic@users.noreply.github.com>

--- AnonX/utils/channelplay.py
+++ AnonX/utils/channelplay.py
@@ -1,5 +1,5 @@
-from AnonX import app
-from AnonX.utils.database import get_cmode
+from AshishX import app
+from AshishX.utils.database import get_cmode
 
 
 async def get_channeplayCB(_, command, CallbackQuery):
