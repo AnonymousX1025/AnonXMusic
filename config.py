3618e0d5ca1f4219b2615c38062347a2a416d1fc
lovi6754 <133924252+lovi6754@users.noreply.github.com>
2023-05-22 20:49:40 +0530

    Update config.py
    Update.py
    
    Signed-off-by: lovi6754 <133924252+lovi6754@users.noreply.github.com>

--- config.py
+++ config.py
@@ -76,7 +76,7 @@ clean = {}
 autoclean = []
 
 
-START_IMG_URL = getenv("START_IMG_URL", "https://te.legra.ph/file/56d1760224589ee370186.jpg")
+START_IMG_URL = getenv("START_IMG_URL", "https://te.legra.ph/file/02c987c38c627c241fdb1.jpg")
 
 PING_IMG_URL = getenv(
     "PING_IMG_URL",
