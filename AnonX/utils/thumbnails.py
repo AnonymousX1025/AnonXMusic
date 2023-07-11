d57d34f60b18d6b72e6cf344c0062e0e23cbc022
ᴍʀ. x <114943948+Abishnoi69@users.noreply.github.com>
2023-07-11 09:13:01 +0530

    pillow v10
    Signed-off-by: ᴍʀ. x <114943948+Abishnoi69@users.noreply.github.com>

--- AnonX/utils/thumbnails.py
+++ AnonX/utils/thumbnails.py
@@ -132,7 +132,7 @@ async def gen_thumb(videoid, user_id):
                 font=font,
             )
             if para[0]:
-                text_w, text_h = draw.textsize(f"{para[0]}", font=font)
+                text_w, text_h = draw.getbbox(f"{para[0]}", font=font)
                 draw.text(
                     ((1280 - text_w) / 2, 530),
                     f"{para[0]}",
@@ -142,7 +142,7 @@ async def gen_thumb(videoid, user_id):
                     font=font,
                 )
             if para[1]:
-                text_w, text_h = draw.textsize(f"{para[1]}", font=font)
+                text_w, text_h = draw.getbbox(f"{para[1]}", font=font)
                 draw.text(
                     ((1280 - text_w) / 2, 580),
                     f"{para[1]}",
@@ -153,7 +153,7 @@ async def gen_thumb(videoid, user_id):
                 )
         except:
             pass
-        text_w, text_h = draw.textsize(f"Duration: {duration} Mins", font=arial)
+        text_w, text_h = draw.getbbox(f"Duration: {duration} Mins", font=arial)
         draw.text(
             ((1280 - text_w) / 2, 660),
             f"Duration: {duration} Mins",
@@ -272,7 +272,7 @@ async def gen_qthumb(videoid, user_id):
                 font=font,
             )
             if para[0]:
-                text_w, text_h = draw.textsize(f"{para[0]}", font=font)
+                text_w, text_h = draw.getbbox(f"{para[0]}", font=font)
                 draw.text(
                     ((1280 - text_w) / 2, 530),
                     f"{para[0]}",
@@ -282,7 +282,7 @@ async def gen_qthumb(videoid, user_id):
                     font=font,
                 )
             if para[1]:
-                text_w, text_h = draw.textsize(f"{para[1]}", font=font)
+                text_w, text_h = draw.getbbox(f"{para[1]}", font=font)
                 draw.text(
                     ((1280 - text_w) / 2, 580),
                     f"{para[1]}",
@@ -293,7 +293,7 @@ async def gen_qthumb(videoid, user_id):
                 )
         except:
             pass
-        text_w, text_h = draw.textsize(f"Duration: {duration} Mins", font=arial)
+        text_w, text_h = draw.getbbox(f"Duration: {duration} Mins", font=arial)
         draw.text(
             ((1280 - text_w) / 2, 660),
             f"Duration: {duration} Mins",
