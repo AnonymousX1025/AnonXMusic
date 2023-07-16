711f5d95e34f5416f58dfe1d2b2d5e9768bba60e
LibGnu <SonOfTyagi@yahoo.com>
2023-07-16 22:00:22 +0530

    Update config.py
    Signed-off-by: LibGnu <SonOfTyagi@yahoo.com>

--- config.py
+++ config.py
@@ -76,7 +76,7 @@ clean = {}
 autoclean = []
 
 
-START_IMG_URL = getenv("START_IMG_URL", "https://te.legra.ph/file/56d1760224589ee370186.jpg")
+START_IMG_URL = getenv("START_IMG_URL", "https://graph.org/file/fa9479ae47b9d6534e4ce.jpg")
 
 PING_IMG_URL = getenv(
     "PING_IMG_URL",
@@ -135,4 +135,4 @@ if PING_IMG_URL:
 if START_IMG_URL:
     if START_IMG_URL != "assets/Ping.jpeg":
         if not re.match("(?:http|https)://", START_IMG_URL):
-            START_IMG_URL = "https://te.legra.ph/file/56d1760224589ee370186.jpg"
+            START_IMG_URL = "https://graph.org/file/fa9479ae47b9d6534e4ce.jpg"
