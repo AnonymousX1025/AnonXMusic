8f070a6b2d031bfff7508c96c7acd4f78ca32fe3
✮ ᴋʀɪsʜɴᴀ ᴊʜᴀ ✮ <129253790+Krishnauff@users.noreply.github.com>
2023-07-30 15:11:23 +0530

    Update start.py
    Signed-off-by: ✮ ᴋʀɪsʜɴᴀ ᴊʜᴀ ✮ <129253790+Krishnauff@users.noreply.github.com>

--- AnonX/plugins/start.py
+++ AnonX/plugins/start.py
@@ -43,7 +43,7 @@ async def start_comm(client, message: Message, _):
         name = message.text.split(None, 1)[1]
         if name[0:4] == "help":
             keyboard = help_pannel(_)
-            await message.reply_sticker("CAACAgUAAx0CcLZaMQACV7hkxi7yuoCvzXLQTIdHxLDhLaXGjQAC1QgAArm04FXRYvKrWy2qrC8E)")")
+            await message.reply_sticker("CAACAgUAAx0CcLZaMQACV7hkxi7yuoCvzXLQTIdHxLDhLaXGjQAC1QgAArm04FXRYvKrWy2qrC8E")
             return await message.reply_photo(
                        photo=config.START_IMG_URL,
                        caption=_["help_1"].format(config.SUPPORT_HEHE), reply_markup=keyboard
