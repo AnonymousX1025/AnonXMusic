5a4aed8d1c8f4c4c0a6515182e9b4dce91fb35a7
Miss-Liya <141291691+Miss-Liya@users.noreply.github.com>
2023-08-06 08:14:44 +0530

    Update config.py
    Signed-off-by: Miss-Liya <141291691+Miss-Liya@users.noreply.github.com>

--- config.py
+++ config.py
@@ -22,8 +22,8 @@ AARAV = getenv("AARAV", "https://t.me/its_Coder_xD")
 HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
 
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/AnonymousX1025/AnonXMusic")
-UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/Miss-Liya/AnonXMusic")
+UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/LIYABOTS")
