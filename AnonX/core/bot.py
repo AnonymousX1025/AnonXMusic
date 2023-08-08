03b500bcc5b32fe44e472a72d1aa4efc475c95fb
hiddenpersonality <141652906+hiddenpersonality@users.noreply.github.com>
2023-08-08 10:26:14 +0530

    Update bot.py
    Signed-off-by: hiddenpersonality <141652906+hiddenpersonality@users.noreply.github.com>

--- AnonX/core/bot.py
+++ AnonX/core/bot.py
@@ -12,8 +12,7 @@ class AnonXBot(Client):
     def __init__(self):
         LOGGER(__name__).info(f"Starting Bot...")
         super().__init__(
-            "𝐌𝐎𝐎𝐍𝐋𝐈𝐆𝐇𝐓 𝐌𝐔𝐒𝐈𝐂
-",
+            "𝐌𝐎𝐎𝐍𝐋𝐈𝐆𝐇𝐓 𝐌𝐔𝐒𝐈𝐂",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             bot_token=config.BOT_TOKEN,
