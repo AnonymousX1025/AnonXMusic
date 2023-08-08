017e946e3e9845a62126103f874ef38ef99d8c99
hiddenpersonality <141652906+hiddenpersonality@users.noreply.github.com>
2023-08-08 11:02:00 +0530

    Update config.py
    Signed-off-by: hiddenpersonality <141652906+hiddenpersonality@users.noreply.github.com>

--- config.py
+++ config.py
@@ -131,9 +131,9 @@ if UPSTREAM_REPO:
 if PING_IMG_URL:
     if PING_IMG_URL != "assets/Ping.jpeg":
         if not re.match("(?:http|https)://", PING_IMG_URL):
-            PING_IMG_URL = "https://telegra.ph/file/56d1760224589ee370186.jpg"
+            PING_IMG_URL = "https://graph.org/file/3e84f03e8e3b9bb0d4df9.jpg"
 
 if START_IMG_URL:
     if START_IMG_URL != "assets/Ping.jpeg":
         if not re.match("(?:http|https)://", START_IMG_URL):
-            START_IMG_URL = "https://te.legra.ph/file/56d1760224589ee370186.jpg"
+            START_IMG_URL = "https://graph.org/file/2a794cf3556a1f5ef8d93.jpg"
