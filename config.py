983c9e3a559f9cfb2701d6d5338baadff5bee168
hiddenpersonality <141652906+hiddenpersonality@users.noreply.github.com>
2023-08-08 13:59:34 +0530

    Update config.py
    Signed-off-by: hiddenpersonality <141652906+hiddenpersonality@users.noreply.github.com>

--- config.py
+++ config.py
@@ -22,7 +22,7 @@ OWNER_ID = list(map(int, getenv("OWNER_ID", "5920701493 , 5886326864").split()))
 HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
 
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/AnonymousX1025/AnonXMusic")
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/hiddenpersonality/-")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
