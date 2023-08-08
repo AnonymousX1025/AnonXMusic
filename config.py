470e1d21f74a42dcd7fb943048aab60d123c7d71
hiddenpersonality <141652906+hiddenpersonality@users.noreply.github.com>
2023-08-08 10:53:27 +0530

    Update config.py
    Signed-off-by: hiddenpersonality <141652906+hiddenpersonality@users.noreply.github.com>

--- config.py
+++ config.py
@@ -77,11 +77,11 @@ clean = {}
 autoclean = []
 
 
-START_IMG_URL = getenv("START_IMG_URL", "https://te.legra.ph/file/56d1760224589ee370186.jpg")
+START_IMG_URL = getenv("START_IMG_URL", "https://graph.org/file/2a794cf3556a1f5ef8d93.jpg")
 
 PING_IMG_URL = getenv(
     "PING_IMG_URL",
-    "https://telegra.ph/file/56d1760224589ee370186.jpg",
+    "https://graph.org/file/3e84f03e8e3b9bb0d4df9.jpg",
 )
 
 PLAYLIST_IMG_URL = "https://te.legra.ph/file/4ec5ae4381dffb039b4ef.jpg"
