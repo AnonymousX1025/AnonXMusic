4699e230e11152c738f6ed67afc29f77a43b8733
ᴅᴇᴠᴀɴsʜ <125127646+Devansh-Bots@users.noreply.github.com>
2023-09-22 21:57:54 +0530

    Update config.py
    Signed-off-by: ᴅᴇᴠᴀɴsʜ <125127646+Devansh-Bots@users.noreply.github.com>

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/AnonymousX1025/AnonXMusic",
+    "https://github.com/Avonplays/AnonXMusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
