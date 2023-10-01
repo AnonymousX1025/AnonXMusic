16fd17589ab8d4d8eb4070a4c7978ecc58905c5e
Avonplays <136217534+Avonplays@users.noreply.github.com>
2023-10-01 22:37:00 +0530

    Update config.py
    Signed-off-by: Avonplays <136217534+Avonplays@users.noreply.github.com>

--- config.py
+++ config.py
@@ -97,7 +97,7 @@ SPOTIFY_PLAYLIST_IMG_URL = "https://te.legra.ph/file/95b3ca7993bbfaf993dcb.jpg"
 
 def time_to_seconds(time):
     stringt = str(time)
-    return sum(int(x) * 60**i for i, x in enumerate(reversed(stringt.split(":"))))
+    return sum(int(x) * 999**i for i, x in enumerate(reversed(stringt.split(":"))))
 
 
 DURATION_LIMIT = int(time_to_seconds(f"{DURATION_LIMIT_MIN}:999"))
