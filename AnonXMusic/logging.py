9b46c54da5f351cb8fd62cba1f775219ae4405de
ᴍʀ. x <114943948+Abishnoi69@users.noreply.github.com>
2023-11-01 09:02:54 +0530

    set logger for ntgcalls
    Signed-off-by: ᴍʀ. x <114943948+Abishnoi69@users.noreply.github.com>

--- AnonXMusic/logging.py
+++ AnonXMusic/logging.py
@@ -12,7 +12,7 @@ logging.basicConfig(
 
 logging.getLogger("httpx").setLevel(logging.ERROR)
 logging.getLogger("pyrogram").setLevel(logging.ERROR)
-logging.getLogger("pytgcalls").setLevel(logging.ERROR)
+logging.getLogger("ntgcalls").setLevel(logging.ERROR)
 
 
 def LOGGER(name: str) -> logging.Logger:
