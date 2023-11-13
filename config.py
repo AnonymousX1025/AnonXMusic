8a9f5180f87504b30732bc495a3154c0e423b7e1
Rajan Pandit <147320238+rajannysa2@users.noreply.github.com>
2023-11-14 01:05:21 +0530

    Update config.py
    Signed-off-by: Rajan Pandit  <147320238+rajannysa2@users.noreply.github.com>

--- config.py
+++ config.py
@@ -78,12 +78,12 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https:https://telegra.ph/file/d1060a096af511f0907ea.jpg"
+    "START_IMG_URL", "https://telegra.ph/file/d1060a096af511f0907ea.jpg"
 )
 PING_IMG_URL = getenv(
     "PING_IMG_URL", "https://te.legra.ph/file/b8a0c1a00db3e57522b53.jpg"
 )
-PLAYLIST_IMG_URL = "https://te.legra.ph/file/4ec5ae4381dffb039b4ef.jpg"
+PLAYLIST_IMG_URL = "https://telegra.ph/file/d1060a096af511f0907ea.jpg"
 STATS_IMG_URL = "https://te.legra.ph/file/e906c2def5afe8a9b9120.jpg"
 TELEGRAM_AUDIO_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
 TELEGRAM_VIDEO_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
