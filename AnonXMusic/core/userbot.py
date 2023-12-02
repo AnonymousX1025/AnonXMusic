d48b4b4c4f90f3c7f949cad10af1a99dcafa2699
˹ᴧɴσɴʏᴍσᴜs ꭙ˼ <TheAnonymousX1025@gmail.com>
2023-12-02 21:09:44 +0530

    Ah shit
    Signed-off-by: ˹ᴧɴσɴʏᴍσᴜs ꭙ˼ <TheAnonymousX1025@gmail.com>

--- AnonXMusic/core/userbot.py
+++ AnonXMusic/core/userbot.py
@@ -73,7 +73,7 @@ class Userbot(Client):
             await self.two.start()
             try:
                 await self.two.join_chat("DevilsHeavenMF")
-                await self.one.join_chat("FallenAssociation")
+                await self.two.join_chat("FallenAssociation")
             except:
                 pass
             assistants.append(2)
@@ -94,7 +94,7 @@ class Userbot(Client):
             await self.three.start()
             try:
                 await self.three.join_chat("DevilsHeavenMF")
-                await self.one.join_chat("FallenAssociation")
+                await self.three.join_chat("FallenAssociation")
             except:
                 pass
             assistants.append(3)
@@ -115,7 +115,7 @@ class Userbot(Client):
             await self.four.start()
             try:
                 await self.four.join_chat("DevilsHeavenMF")
-                await self.one.join_chat("FallenAssociation")
+                await self.four.join_chat("FallenAssociation")
             except:
                 pass
             assistants.append(4)
@@ -136,7 +136,7 @@ class Userbot(Client):
             await self.five.start()
             try:
                 await self.five.join_chat("DevilsHeavenMF")
-                await self.one.join_chat("FallenAssociation")
+                await self.five.join_chat("FallenAssociation")
             except:
                 pass
             assistants.append(5)
