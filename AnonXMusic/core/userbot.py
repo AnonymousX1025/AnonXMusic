ae61b260e608fed06b711058162913ae91857bb8
zenaku15 <161702193+zenaku15@users.noreply.github.com>
2024-03-01 07:35:18 +0630

    Update userbot.py
    Signed-off-by: zenaku15 <161702193+zenaku15@users.noreply.github.com>

--- AnonXMusic/core/userbot.py
+++ AnonXMusic/core/userbot.py
@@ -51,8 +51,8 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("DevilsHeavenMF")
-                await self.one.join_chat("FallenAssociation")
+                await self.one.join_chat("seriosvs_version10")
+                await self.one.join_chat("seriousvs_version20")
             except:
                 pass
             assistants.append(1)
@@ -72,8 +72,8 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("DevilsHeavenMF")
-                await self.two.join_chat("FallenAssociation")
+                await self.two.join_chat("seriosvs_version10")
+                await self.two.join_chat("seriousvs_version20")
             except:
                 pass
             assistants.append(2)
@@ -93,8 +93,8 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("DevilsHeavenMF")
-                await self.three.join_chat("FallenAssociation")
+                await self.three.join_chat("seriosvs_version10")
+                await self.three.join_chat("seriousvs_version20")
             except:
                 pass
             assistants.append(3)
@@ -114,8 +114,8 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("DevilsHeavenMF")
-                await self.four.join_chat("FallenAssociation")
+                await self.four.join_chat("seriosvs_version10")
+                await self.four.join_chat("seriousvs_version20")
             except:
                 pass
             assistants.append(4)
@@ -135,8 +135,8 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("DevilsHeavenMF")
-                await self.five.join_chat("FallenAssociation")
+                await self.five.join_chat("seriosvs_version10")
+                await self.five.join_chat("seriousvs_version20")
             except:
                 pass
             assistants.append(5)
