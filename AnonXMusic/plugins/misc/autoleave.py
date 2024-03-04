1dc5a19b3163244939743a4ddbb2d6d0d11d09da
zenaku15 <161702193+zenaku15@users.noreply.github.com>
2024-03-04 09:50:54 +0630

    Update autoleave.py
    Signed-off-by: zenaku15 <161702193+zenaku15@users.noreply.github.com>

--- AnonXMusic/plugins/misc/autoleave.py
+++ AnonXMusic/plugins/misc/autoleave.py
@@ -26,8 +26,8 @@ async def auto_leave():
                         ]:
                             if (
                                 i.chat.id != config.LOGGER_ID
-                                and i.chat.id != -1001686672798
-                                and i.chat.id != -1001549206010
+                                and i.chat.id != -1001835045094
+                                and i.chat.id != -1001983934671
                             ):
                                 if left == 20:
                                     continue
