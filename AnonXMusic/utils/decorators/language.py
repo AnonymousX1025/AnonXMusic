7ff9a08465476ed6a58428f2a8bff67dd5330a77
Deva09876 <125013272+Deva09876@users.noreply.github.com>
2024-04-09 23:36:31 +0530

    Missing import (#128)
    Signed-off-by: Deva09876 <125013272+Deva09876@users.noreply.github.com>

--- AnonXMusic/utils/decorators/language.py
+++ AnonXMusic/utils/decorators/language.py
@@ -2,6 +2,7 @@ from strings import get_string
 
 from AnonXMusic import app
 from AnonXMusic.misc import SUDOERS
+from config import SUPPORT_CHAT
 from AnonXMusic.utils.database import get_lang, is_maintenance
 
 
