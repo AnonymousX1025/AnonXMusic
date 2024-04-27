e05a724193b8c456a8b850f3edd974736ea9b211
innocentbabe <145816333+innocentbabe@users.noreply.github.com>
2024-04-28 02:51:49 +0530

    Update start.py
    Signed-off-by: innocentbabe <145816333+innocentbabe@users.noreply.github.com>

--- AnonXMusic/utils/inline/start.py
+++ AnonXMusic/utils/inline/start.py
@@ -31,7 +31,7 @@ def private_panel(_):
         ],
         [
             InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
-            InlineKeyboardButton(text=_["S_B_7"], url=config.UPSTREAM_REPO),
+            InlineKeyboardButton(text=["Oᴡɴᴇʀ"], url=f"https://t.me/DRDIC"),
         ],
     ]
     return buttons
