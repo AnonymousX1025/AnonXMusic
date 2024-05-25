ff100cb174f7a6eb36c3cd26b7a9b8b6221f48ef
MR NEON <141004134+mrneon1605@users.noreply.github.com>
2024-05-25 11:47:10 +0530

    Update bot.py
    Signed-off-by: MR NEON <141004134+mrneon1605@users.noreply.github.com>

--- AnonX/core/bot.py
+++ AnonX/core/bot.py
@@ -12,7 +12,7 @@ class AnonXBot(Client):
     def __init__(self):
         LOGGER(__name__).info(f"Starting Bot...")
         super().__init__(
-            "AnonXMusic",
+            "LoveXMusic",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             bot_token=config.BOT_TOKEN,
