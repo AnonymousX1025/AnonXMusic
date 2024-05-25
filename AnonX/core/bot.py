1085fa62aa8721ae87ffb3c5f86821c03806d79f
MR NEON <141004134+mrneon1605@users.noreply.github.com>
2024-05-25 11:47:58 +0530

    Update bot.py
    Signed-off-by: MR NEON <141004134+mrneon1605@users.noreply.github.com>

--- AnonX/core/bot.py
+++ AnonX/core/bot.py
@@ -8,7 +8,7 @@ from ..logging import LOGGER
 
 
 
-class AnonXBot(Client):
+class LoveXBot(Client):
     def __init__(self):
         LOGGER(__name__).info(f"Starting Bot...")
         super().__init__(
