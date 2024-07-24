271302fc619dca56352e8276610dc62c7d0ec64b
githubmeking <130077291+githubmeking@users.noreply.github.com>
2024-07-24 18:34:44 +0300

    Update config.py
    Signed-off-by: githubmeking <130077291+githubmeking@users.noreply.github.com>

--- config.py
+++ config.py
@@ -41,7 +41,7 @@ GIT_TOKEN = getenv(
 )  # Fill this variable if your upstream repository is private
 
 SUPPORT_CHANNEL = getenv("Çeşitli Kanallar", "https://t.me/hurkanal")
-SUPPORT_CHAT = getenv("Destek Kanal", "https://t.me/hurhanal")
+SUPPORT_CHAT = getenv("Destek Kanal", "https://t.me/hurkanal")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
