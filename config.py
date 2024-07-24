1f42912ee88a31b604318743e727a1cd88b12863
githubmeking <130077291+githubmeking@users.noreply.github.com>
2024-07-24 18:30:34 +0300

    Update config.py
    Signed-off-by: githubmeking <130077291+githubmeking@users.noreply.github.com>

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/AnonymousX1025/AnonXMusic",
+    "https://t.me/yks_kpss_pdf_2024",
 )
 
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
