ab30492d46bd6760626b8578bc25924408f55b01
githubmeking <130077291+githubmeking@users.noreply.github.com>
2024-07-24 18:10:18 +0300

    Update config.py
    Signed-off-by: githubmeking <130077291+githubmeking@users.noreply.github.com>

--- config.py
+++ config.py
@@ -30,6 +30,10 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # Get it from http://dashboard.heroku.com/account
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
+UPSTREAM_REPO = getenv(
+    "UPSTREAM_REPO",
+    "https://github.com/AnonymousX1025/AnonXMusic",
+)
 
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
