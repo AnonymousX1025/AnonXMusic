32ae6ccca4ae3ef288397289cd6b68d4daf6133a
githubmeking <130077291+githubmeking@users.noreply.github.com>
2024-07-24 15:45:58 +0300

    Update config.py
    Signed-off-by: githubmeking <130077291+githubmeking@users.noreply.github.com>

--- config.py
+++ config.py
@@ -56,8 +56,8 @@ PLAYLIST_FETCH_LIMIT = int(getenv("PLAYLIST_FETCH_LIMIT", 25))
 
 
 # Telegram audio and video file size limit (in bytes)
-TG_AUDIO_FILESIZE_LIMIT = int(getenv("TG_AUDIO_FILESIZE_LIMIT", 104857600))
-TG_VIDEO_FILESIZE_LIMIT = int(getenv("TG_VIDEO_FILESIZE_LIMIT", 1073741824))
+TG_AUDIO_FILESIZE_LIMIT = int(getenv("TG_AUDIO_FILESIZE_LIMIT", 209715200 ))
+TG_VIDEO_FILESIZE_LIMIT = int(getenv("TG_VIDEO_FILESIZE_LIMIT", 2147483648))
 # Checkout https://www.gbmb.org/mb-to-bytes for converting mb to bytes
 
 
