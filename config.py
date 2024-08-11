50ce9c08ef3653c8644d6dc9b4ab59932ab447c3
naya1503 <118755946+naya1503@users.noreply.github.com>
2024-08-11 11:42:24 +0700

    update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/AnonymousX1025/AnonXMusic",
+    "https://github.com/rezavpn/musikanon",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
