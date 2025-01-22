7d354e898a040ae16d7dd2c3507e0e60124b1cee
githubmeking <130077291+githubmeking@users.noreply.github.com>
2025-01-22 22:27:25 +0300

    Update play.py
    Signed-off-by: githubmeking <130077291+githubmeking@users.noreply.github.com>

--- AnonXMusic/plugins/play/play.py
+++ AnonXMusic/plugins/play/play.py
@@ -29,8 +29,7 @@ from config import BANNED_USERS, lyrical
     filters.command(
         [
             "play",
-            "oynat"
-            "voynat"
+            "oynat",
             "vplay",
             "cplay",
             "cvplay",
