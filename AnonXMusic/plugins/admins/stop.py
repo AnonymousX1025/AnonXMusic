e2cc93ee1084cd3e01d38a3a3b81de1541044edd
githubmeking <130077291+githubmeking@users.noreply.github.com>
2025-01-22 22:30:14 +0300

    Update stop.py
    Signed-off-by: githubmeking <130077291+githubmeking@users.noreply.github.com>

--- AnonXMusic/plugins/admins/stop.py
+++ AnonXMusic/plugins/admins/stop.py
@@ -10,7 +10,7 @@ from config import BANNED_USERS
 
 
 @app.on_message(
-    filters.command(["end", "stop", "cend", "cstop"]) & filters.group & ~BANNED_USERS
+    filters.command(["end", "stop", "cend", "cstop","son","bitir"]) & filters.group & ~BANNED_USERS
 )
 @AdminRightsCheck
 async def stop_music(cli, message: Message, _, chat_id):
