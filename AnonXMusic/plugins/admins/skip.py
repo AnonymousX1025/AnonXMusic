0dfc362e9f4c712f74e7899db899ffff79d5aa70
githubmeking <130077291+githubmeking@users.noreply.github.com>
2025-01-22 22:30:40 +0300

    Update skip.py
    Signed-off-by: githubmeking <130077291+githubmeking@users.noreply.github.com>

--- AnonXMusic/plugins/admins/skip.py
+++ AnonXMusic/plugins/admins/skip.py
@@ -14,7 +14,7 @@ from config import BANNED_USERS
 
 
 @app.on_message(
-    filters.command(["skip", "cskip", "next", "cnext"]) & filters.group & ~BANNED_USERS
+    filters.command(["skip", "cskip", "next", "cnext","atla"]) & filters.group & ~BANNED_USERS
 )
 @AdminRightsCheck
 async def skip(cli, message: Message, _, chat_id):
