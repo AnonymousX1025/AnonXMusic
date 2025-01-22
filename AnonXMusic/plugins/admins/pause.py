9c6e67c0bb08a1fdc395b332992d0a83aa285fab
githubmeking <130077291+githubmeking@users.noreply.github.com>
2025-01-22 22:30:57 +0300

    Update pause.py
    Signed-off-by: githubmeking <130077291+githubmeking@users.noreply.github.com>

--- AnonXMusic/plugins/admins/pause.py
+++ AnonXMusic/plugins/admins/pause.py
@@ -9,7 +9,7 @@ from AnonXMusic.utils.inline import close_markup
 from config import BANNED_USERS
 
 
-@app.on_message(filters.command(["pause", "cpause"]) & filters.group & ~BANNED_USERS)
+@app.on_message(filters.command(["pause", "cpause","dur"]) & filters.group & ~BANNED_USERS)
 @AdminRightsCheck
 async def pause_admin(cli, message: Message, _, chat_id):
     if not await is_music_playing(chat_id):
