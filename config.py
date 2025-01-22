5e031d9889934e2c1509d7d74e20a820a5d1f4fd
githubmeking <130077291+githubmeking@users.noreply.github.com>
2025-01-22 21:50:51 +0300

    Update config.py
    Signed-off-by: githubmeking <130077291+githubmeking@users.noreply.github.com>

--- config.py
+++ config.py
@@ -22,7 +22,7 @@ DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 60))
 LOGGER_ID = int(getenv("LOGGER_ID", -1002143343100))
 
 # Get this value from @FallenxBot on Telegram by /id
-OWNER_ID = int(getenv("OWNER_ID", 6180151150))
+OWNER_ID = int(getenv("OWNER_ID", 5920592740))
 
 ## Fill these variables if you're deploying on heroku.
 # Your heroku app name
