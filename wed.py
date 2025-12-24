2ea5c6ee3066c9b993b8f7412774ea355ba1735e
Ahmed Mido <ahmedmidonew@outlook.com>
2025-12-24 18:39:35 +0200

    Update wed.py
    Signed-off-by: Ahmed Mido <ahmedmidonew@outlook.com>

--- wed.py
+++ wed.py
@@ -1,11 +1,12 @@
-import os
 from flask import Flask
+import os
 
-app = Flask(__name__)
+app = Flask("anony")
 
 @app.route("/")
 def home():
-    return "Bot is running"
+    return "Bot is alive!"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
