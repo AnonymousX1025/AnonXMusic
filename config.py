0023978f4535452ed880c1451dde68a99f6f81b7
Xeyaldi <167771989+Xeyaldi@users.noreply.github.com>
2026-01-21 22:15:28 +0400

    Update support channel and chat URLs
    Signed-off-by: Xeyaldi <167771989+Xeyaldi@users.noreply.github.com>

--- config.py
+++ config.py
@@ -22,8 +22,8 @@ class Config:
         self.SESSION2 = getenv("SESSION2", None)
         self.SESSION3 = getenv("SESSION3", None)
 
-        self.SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/FallenAssociation")
-        self.SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/DevilsHeavenMF")
+        self.SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/ht_bots")
+        self.SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/ht_bots_chat")
 
         self.AUTO_END: bool = getenv("AUTO_END", False)
         self.AUTO_LEAVE: bool = getenv("AUTO_LEAVE", False)
