f19b1472109bdede46e0bb6abaa577f6fe355e1c
Xeyaldi <167771989+Xeyaldi@users.noreply.github.com>
2026-01-23 20:23:57 +0400

    Update image URLs for PING_IMG and START_IMG
    Signed-off-by: Xeyaldi <167771989+Xeyaldi@users.noreply.github.com>

--- config.py
+++ config.py
@@ -33,8 +33,8 @@ class Config:
             if url and "batbin.me" in url
         ]
         self.DEFAULT_THUMB = getenv("DEFAULT_THUMB", "https://te.legra.ph/file/3e40a408286d4eda24191.jpg")
-        self.PING_IMG = getenv("PING_IMG", "https://files.catbox.moe/haagg2.png")
-        self.START_IMG = getenv("START_IMG", "https://files.catbox.moe/zvziwk.jpg")
+        self.PING_IMG = getenv("PING_IMG", "https://files.catbox.moe/dp714e.png")
+        self.START_IMG = getenv("START_IMG", "https://files.catbox.moe/kjrbtz.jpg")
 
     def check(self):
         missing = [
