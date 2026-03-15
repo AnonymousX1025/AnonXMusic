1ecf11841d7f6b31399a8dd732630adcbd5058c9
anony <notanony@hotmail.com>
2026-03-15 22:03:27 +0530

    Typo
    Signed-off-by: anony <notanony@hotmail.com>

--- anony/helpers/_play.py
+++ anony/helpers/_play.py
@@ -35,7 +35,7 @@ def checkUB(play):
         video = m.command[0][0] == "v" and config.VIDEO_PLAY
         url = utils.get_url(m)
         if url and yt.invalid(url):
-            return await m.reply_text(m.lang["play_not_found"])
+            return await m.reply_text(m.lang["play_not_found"].format(config.SUPPORT_CHAT))
         m3u8 = url and not yt.valid(url)
 
         play_mode = await db.get_play_mode(chat_id)
