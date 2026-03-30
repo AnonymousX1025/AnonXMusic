e581e3bca7a3f7f692ca3ef9c0d9dae95f4e6cb9
hikipi22-lgtm <hikipi22@gmail.com>
2026-03-30 14:10:11 +0530

    Update autoplay.py
    Signed-off-by: hikipi22-lgtm <hikipi22@gmail.com>

--- anony/plugins/autoplay.py
+++ anony/plugins/autoplay.py
@@ -1,24 +1,10 @@
 from pyrogram import filters
 from pyrogram.types import Message
 from anony import app
-
-# Sabse safe tarika database import karne ka
-try:
-    from anony.utils.database import is_autoplay_on, autoplay_on, autoplay_off
-except:
-    try:
-        from utils.database import is_autoplay_on, autoplay_on, autoplay_off
-    except:
-        try:
-            from ..utils.database import is_autoplay_on, autoplay_on, autoplay_off
-        except:
-            is_autoplay_on = None
+from ..utils.database import is_autoplay_on, autoplay_on, autoplay_off
 
 @app.on_message(filters.command(["autoplay"]) & ~filters.private)
 async def autoplay_stat(_, message: Message):
-    if is_autoplay_on is None:
-        return await message.reply_text("❌ Database structure not found in your repo.")
-    
     if len(message.command) != 2:
         return await message.reply_text("Usage:\n/autoplay [on|off]")
     
@@ -32,3 +18,4 @@ async def autoplay_stat(_, message: Message):
         await message.reply_text("❌ **Autoplay Disabled!**")
     else:
         await message.reply_text("Invalid! Use `/autoplay on` or `/autoplay off`.")
+
