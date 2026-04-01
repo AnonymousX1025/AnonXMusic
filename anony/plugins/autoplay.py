8486f8b1e95ca1b88bd07eb88dcdb68382c65022
hikipi22-lgtm <hikipi22@gmail.com>
2026-04-01 19:43:53 +0530

    Update autoplay.py
    Signed-off-by: hikipi22-lgtm <hikipi22@gmail.com>

--- anony/plugins/autoplay.py
+++ anony/plugins/autoplay.py
@@ -1,17 +1,17 @@
 from pyrogram import filters
 from anony import app
-# Yahan 'utils' hata kar direct 'database' se import kar rahe hain
-from database import is_autoplay_mode, autoplay_on, autoplay_off
+from anony.utils.database import is_autoplay_mode, autoplay_on, autoplay_off
 
 @app.on_message(filters.command(["autoplay"]))
 async def autoplay_mnt(_, message):
     usage = "Usage:\n\n/autoplay [enable|disable]"
     if len(message.command) != 2:
         return await message.reply_text(usage)
+    
     state = message.text.split(None, 1)[1].strip().lower()
     if state == "enable":
         await autoplay_on(message.chat.id)
-        await message.reply_text("✅ Autoplay Enabled!")
+        await message.reply_text("✅ Autoplay Enabled! Ab gaane apne aap chalenge.")
     elif state == "disable":
         await autoplay_off(message.chat.id)
         await message.reply_text("❌ Autoplay Disabled!")
