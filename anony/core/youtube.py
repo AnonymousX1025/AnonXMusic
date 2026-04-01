6eed395e25ded1060cd3f17852a85fa0c342ffb9
hikipi22-lgtm <hikipi22@gmail.com>
2026-04-02 02:03:59 +0530

    Update youtube.py
    Signed-off-by: hikipi22-lgtm <hikipi22@gmail.com>

--- anony/core/youtube.py
+++ anony/core/youtube.py
@@ -155,7 +155,6 @@ class YouTube:
             "overwrites": False,
             "nocheckcertificate": True,
             "cookiefile": cookie,
-            # User-Agent add kiya taaki bot block na ho
             "user_agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36"
         }
 
