4f40878e509c55acc17978ad955259d43d06b73e
yfree1232-afk <yfree1232@gmail.com>
2026-05-15 23:55:28 +0530

    Update support channel and chat URLs
    Signed-off-by: yfree1232-afk <yfree1232@gmail.com>

--- config.py
+++ config.py
@@ -22,8 +22,8 @@ class Config:
         self.SESSION2 = getenv("SESSION2", None)
         self.SESSION3 = getenv("SESSION3", None)
 
-        self.SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/fallenx")
-        self.SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/DevilsHeavenMF")
+        self.SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/mhginny")
+        self.SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/khusuphur")
 
         self.AUTO_LEAVE: bool = getenv("AUTO_LEAVE", "False").lower() == "true"
         self.AUTO_END: bool = getenv("AUTO_END", "False").lower() == "true"
